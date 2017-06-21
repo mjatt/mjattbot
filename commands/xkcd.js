5821962f9bae40ae8cff38378ed2a853ae5e58f3
mjatt <taggartm@live.co.uk>
2017-06-21 15:18:00 +0100

    working xkcd comics, random, latest and specific

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -1,7 +1,41 @@
-var xkcd = require('xkcd');
+var xkcd = require('xkcd-api');
+var minimist = require('minimist');
+
 exports.run = (client, message, args) => {
 
-  xkcd(function (data) {
-  message.channel.send(data.img);
-});
+  if(args[0] == null) {
+    xkcd.latest(function (error, response) {
+      if (error) {
+        console.error(error);
+      } else {
+         message.channel.send(`**Title:** ${response.safe_title}`);
+         message.channel.send(response.img);
+         message.channel.send(`**Comic number:** ${response.num}, **Made in:** ${response.year}`);
+      }
+  });
+} else if (args[0] == "random") {
+  xkcd.random(function (error, response) {
+    if (error) {
+      console.error(error);
+      message.channel.send(error);
+    } else {
+      message.channel.send(`**Title:** ${response.safe_title}`);
+      message.channel.send(response.img);
+      message.channel.send(`**Comic number:** ${response.num}, **Made in:** ${response.year}`);
+    }
+  })
+} else {
+  let number = args[0]
+
+    xkcd.get(number, function (error, response) {
+      if (error) {
+        console.error(error);
+        message.channel.send(error);
+      } else {
+      message.channel.send(`**Title:** ${response.safe_title}`);
+      message.channel.send(response.img);
+      message.channel.send(`**Comic number:** ${response.num}, **Made in:** ${response.year}`);
+    }
+  })
 }
+};
