256db296d8eca9508b51fbd78b5857d9d18ac88b
mjatt <taggartm@live.co.uk>
2017-06-21 16:27:10 +0100

    fixed purge errors

--- commands/purge.js
+++ commands/purge.js
@@ -13,6 +13,6 @@ exports.run = (client, message, args) => {
     let messagecount = parseInt(args[0]) + 1;
     message.channel.fetchMessages({limit: messagecount})
         .then(messages => message.channel.bulkDelete(messages));
-    }
-  }
-}
+    };
+  };
+};
