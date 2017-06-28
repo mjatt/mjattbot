7edcbabb9a7bbd58fb0df47410681a4b5470cd3f
mjatt <taggartm@live.co.uk>
2017-06-28 12:59:14 +0100

    deprecation changes

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = (client, member) => {
   let guild = member.guild; //gets the server that the member joined
-  guild.defaultChannel.sendMessage(
+  guild.defaultChannel.send(
     `Welcome to ${member.guild.name}, ${member.user}!`
   ); //sends message to default channel of the server
   console.log(`${member.user} just joined ${member.guild.name}`);
