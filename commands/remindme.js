baaa2b5029162291f6880d3a734c5fde00bd1c7c
mjatt <taggartm@live.co.uk>
2017-07-10 12:15:30 +0100

    hotfix: added NaN check

--- commands/remindme.js
+++ commands/remindme.js
@@ -3,6 +3,8 @@ exports.run = (client, message, args) => {
       message.channel.send(`Specify a length of time to remind you in minutes, e.g. 60`);
     } else if(args[1] == null) {
       message.channel.send(`Specify what I should remind you about`);
+    } else if(isNaN(args[0])) {
+      message.channel.send(`Invalid input, first argument should be a number`);
     } else {
       let value = args[0];
       let final = value * 60000;
