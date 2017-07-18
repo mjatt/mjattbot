d03f7f831f6af57bdeb84cb7737a587d6ee9348a
mjatt <taggartm@live.co.uk>
2017-07-18 09:44:33 +0100

    disabled welcome message until +enable works

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,19 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+var json = [{
+  "id":"181839332313792518",
+  "msg":"enabled"
+}];
 
 exports.run = (client, member) => {
+/*for (var i = 0; i < json.length; i++) {
+  var obj = json[i];
+
+  console.log(obj.id);
+}
+
   let guild = member.guild; //gets the server that the member joined
   guild.defaultChannel.send(
     `Welcome to ${member.guild.name}, ${member.user}!`
   ); //sends message to default channel of the server
-  console.log(`${member.user} just joined ${member.guild.name}`);
+  console.log(`${member.user} just joined ${member.guild.name}`); */
 };
