481aa949da525392889cd3d357b7ef83116cc219
mjatt <taggartm@live.co.uk>
2017-08-03 08:30:49 +0100

    small tweak for more fun

--- commands/stab.js
+++ commands/stab.js
@@ -3,12 +3,35 @@ exports.run = (client, message, args) => {
     message.channel.send(`You haven't specified anyone to stab!`);
   } else {
     let victim = args.join(" ");
-    var number = ["1", "2"];
+    var number = ["1", "2", "3", "4", "5", "6"];
     var answer = number[Math.floor(Math.random() * number.length)];
-    if (answer === "1") {
+    if (message.author.id === `122088742910558209`) {
+      message.reply(
+        `the almighty Mjatt stabbed ${victim} with his finger. ${victim} died`
+      );
+    } else if (answer === "1") {
       message.reply(`stabbed ${victim}, rippo my gyppo`);
     } else if (answer === "2") {
       message.reply(`You messed up and stabbed yourself.`);
+    } else if (answer === "3") {
+      message.reply(
+        `You stabbed ${victim} with a balloon. They stabbed you with a knife.`
+      );
+    } else if (answer === "4") {
+      message.reply(`You stabbed ${victim}, but got caught by the police.`);
+    } else if (answer === "5") {
+      message.reply(`You stabbed ${victim}'s toe. They're very upset.`);
+    } else if (answer === "6") {
+      var filter = m => m.content.startsWith(`q`);
+
+      message.reply(`Quick! Press Q to stab ${victim}!`);
+      message.channel
+        .awaitMessages(filter, { max: 1, time: 2000, errors: ["time"] })
+        .then(function(collected) {
+          var x = collected.first().author.username;
+          message.channel.send(`${x} stabbed ${victim}! Someone save us all!`);
+        })
+        .catch(collected => message.channel.send(`You missed ${victim}.`));
     }
   }
 };
