183153bd827506941330970922da1f6236991d86
mjatt <taggartm@live.co.uk>
2017-08-03 08:30:15 +0100

    added reasoning to ban/kicks

--- commands/ban.js
+++ commands/ban.js
@@ -8,7 +8,12 @@ exports.run = (client, message, args) => {
     message.reply(`Insufficient permissions`);
   } else {
     let banUser = message.mentions.users.first();
+    let reason = args.slice(1).join(" ");
     if (!banUser) return message.reply(`Specify a user to ban`);
+    banUser.send(
+      `You have been banned from ${message.guild} by ${message.author.username}. Reason: ${reason}`
+    );
+    banUser.send(`http://gph.is/2gdmhIL`);
     message.guild.member(banUser).kick();
     message.reply(`Banned ${banUser}`);
   }
--- commands/kick.js
+++ commands/kick.js
@@ -8,8 +8,13 @@ exports.run = (client, message, args) => {
     message.reply(`Insufficient permissions`);
   } else {
     let kickUser = message.mentions.users.first();
+    let reason = args.slice(1).join(" ");
     if (!kickUser) return message.reply(`Specify a user to kick`);
+    kickUser.send(
+      `You have been kicked from ${message.guild} by ${message.author.username}. Reason: ${reason}`
+    );
+    kickUser.send(`http://gph.is/2gdmhIL`);
     message.guild.member(kickUser).kick();
     message.reply(`Kicked ${kickUser}`);
-  };
-}
+  }
+};
