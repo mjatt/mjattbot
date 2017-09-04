9330f722da60f63775dad0d8307e9b42a1398af1
mjatt <taggartm@live.co.uk>
2017-09-04 18:05:02 +0100

    fixed play when not using valid url

--- commands/play.js
+++ commands/play.js
@@ -1,50 +1,81 @@
+var validator = require('youtube-validator')
+
 exports.run = (client, message, args) => {
-  const url = args.join(" ");
+  const url = args[0];
   const yt = require(`ytdl-core`);
   const voiceChannel = message.member.voiceChannel;
   if (!args[0]) return message.reply(`Please specify a link to play`);
   if (!voiceChannel) {
     return message.reply(`You must be in a voice channel first`);
   }
-  voiceChannel.join().then(connnection => {
-    dispatcher = message.guild.voiceConnection.playStream(
-      yt(url, {
-        audioonly: true
-      }),
-      {
-        passes: 1
+
+  if (url.charAt(4) === "s") {
+    var https = url.substring(8);
+  } else {
+    var http = url.substring(7);
+  }
+  if (!https) {
+    validator.validateUrl(http, function (res, err) {
+      if (err) {
+        console.log(err);
+        message.channel.send(`Error! Are you sure you've used a valid YouTube URL?`);
+      } else {
+        hello();
       }
-    );
-    yt.getInfo(url, function(err, info) {
-      message.channel.send(
-        `Playing **${info.title}**! Use: .pause .resume and .stop to control playback.`
+    })
+  } else {
+    validator.validateUrl(https, function (res, err) {
+      if (err) {
+        console.log(err);
+        message.channel.send(`Error! Are you sure you've used a valid YouTube URL?`);
+      } else {
+        hello();
+      }
+    })
+  }
+
+  function hello() {
+    voiceChannel.join().then(connnection => {
+      dispatcher = message.guild.voiceConnection.playStream(
+        yt(url, {
+          audioonly: true
+        }),
+        {
+          passes: 1
+        }
       );
-    });
-    let collector = message.channel.createCollector(m => m);
-    collector.on("collect", c => {
-      if (c.content.startsWith(".pause")) {
-        message.channel.send("Song paused").then(() => {
-          dispatcher.pause();
-        });
-      } else if (c.content.startsWith(".resume")) {
-        message.channel.send("Song resumed").then(() => {
-          dispatcher.resume();
-        });
-      } else if (c.content.startsWith(".stop")) {
-        message.channel.send("Stopped").then(() => {
-          dispatcher.end();
-        });
-      }
-    });
-    dispatcher.on("end", () => {
-      collector.stop();
-      voiceChannel.leave();
-    });
-    dispatcher.on("error", err => {
-      return message.channel.send("Error: " + err).then(() => {
+      yt.getInfo(url, function (err, info) {
+        message.channel.send(
+          `Playing **${info.title}**! Use: .pause .resume and .stop to control playback.`
+        );
+      });
+      let collector = message.channel.createCollector(m => m);
+      collector.on("collect", c => {
+        if (c.content.startsWith(".pause")) {
+          message.channel.send("Song paused").then(() => {
+            dispatcher.pause();
+          });
+        } else if (c.content.startsWith(".resume")) {
+          message.channel.send("Song resumed").then(() => {
+            dispatcher.resume();
+          });
+        } else if (c.content.startsWith(".stop")) {
+          message.channel.send(`Stopped playback in ${voiceChannel}`).then(() => {
+            // dispatcher.end();
+            voiceChannel.leave();
+          });
+        }
+      });
+      dispatcher.on("end", () => {
         collector.stop();
         voiceChannel.leave();
       });
+      dispatcher.on("error", err => {
+        voiceChannel.leave();
+        return message.channel.send("Error: " + err).then(() => {
+          collector.stop();
+        });
+      });
     });
-  });
+  }
 };
