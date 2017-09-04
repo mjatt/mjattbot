8ee13a879d32bde14a16fb730f5b3ce24c0ee137
ModzOn <modzongaming@gmail.com>
2017-09-04 19:14:25 +0200

    runs tag command now if there's an unknown command
    runs tag command now if there's an unknown command
    some cleanup: only get `Client` from djs as in this file, only `Client` is used (don't judge me on what i call clean)
    catching promise rejections at the .login() function for invalid token logins, YES client.login() RETURNS A PROMISE
    added at the unhandled Rejection catcher a reminder to catch promise rejections

--- mjattbot.js
+++ mjattbot.js
@@ -1,5 +1,7 @@
-const Discord = require("discord.js"); //allows discordjs to work
-const client = new Discord.Client(); //initiates the bot
+const { Client } = require("discord.js"); //allows discordjs to work. only require Client, because there aren't any other options used in this file
+const client = new Client({
+  disableEveryone: true /* makes sure that the bot cannot mention everyone */
+}); //initiates the bot
 const yt = require("ytdl-core"); //allows for youtube integration
 const key = require("./config.json"); //contains the prefix and bot token
 const firebase = require("firebase");
@@ -58,33 +60,38 @@ function pokeCatch() {
 }
 
 client.on("message", message => {
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") {
-    message.channel.send(
-      "Please try this in a public guild. Error: DM channel"
-    );
-    return;
-  }
-  if (!message.content.startsWith(key.prefix)) return;
+  if (message.author.bot) return; // ignore bots to avoid infinite loop
+  if (message.channel.type === "dm") return message.channel.send("Please try this in a public guild. Error: DM channel"); // cleanup, code looks bit cleaner here imo
+  
+  if (!message.content.startsWith(key.prefix)) return; // ignore msgs without prefix
 
   let command = message.content.split(" ")[0];
   command = command.slice(key.prefix.length);
+  // console.log(command);
 
   let args = message.content.split(" ").slice(1);
 
   try {
     let cmd = require(`./commands/${command}.js`);
     cmd.run(client, message, args);
-    count++;
-    console.log(
-      `Message Number: ${count} Command: ${command} Args: ${args.join(" ")} Author: ${message.author.username}`
-    );
   } catch (err) {
-    console.log(err);
+    try {
+      let tagcmd = require(`./commands/tag.js`); // require the tag command
+      let args = message.content.replace(key.prefix, "").split(" "); // makes sure that in this case, args for the tag command is the message content without the prefix
+      // console.log(args);
+      tagcmd.run(client, message, args);
+    } catch (err) {
+      console.error(err);
+    }
   }
 });
-client.login(key.token); //the token used to initiate the bot, KEEP SECRET PLS
+client.login(key.token).then(() => {
+  console.info(`Logged in successfully as ${client.user.tag}`);
+}).catch(err => console.error("Failed to login: " + err)); /* the token used to initiate the bot, KEEP SECRET PLS 
+also added catching error with failing login
+*/
 
 process.on("unhandledRejection", err => {
-  console.error("Unhandled exception: " + err.stack);
+  console.error("Unhandled promise rejection, make sure to catch promise rejections!\n " + err.stack); /* added a reminder
+  to not forget to catch promise rejections*/
 });
